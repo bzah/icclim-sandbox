deb9cabd303943963b06ce3e90a7da4cab68a766
abel <aoun@cerfacs.fr>
2021-10-29 14:00:39 +0200

    Fix small issues

--- icclim-run-vs-climpact-files.py
+++ icclim-run-vs-climpact-files.py
@@ -2,22 +2,22 @@ import datetime
 import glob
 import time
 
+import eca_indices
 import numpy as np
+import xarray as xr
+from distributed import Client
 from matplotlib import pyplot as plt
 
 import icclim
-from distributed import Client
-import eca_indices
-import xarray as xr
 
 
 def convert_unit(da, var_name: str):
     if var_name in ["dtr"]:
         return da
     if da.attrs.get("units", None) == "degrees_C":
-        return da + 272.15
-    if da.dtype == np.dtype('timedelta64[ns]'):
-        return da / np.timedelta64(1, 'D')
+        return da + 273.15
+    if da.dtype == np.dtype("timedelta64[ns]"):
+        return da / np.timedelta64(1, "D")
     return da
 
 
@@ -26,7 +26,7 @@ def run():
 
     files = "netcdf_files/climpact.sampledata.gridded.1991-2010.nc"
 
-    Client(memory_limit='8GB', timeout=20, n_workers=1, threads_per_worker=8)
+    Client(memory_limit="8GB", timeout=20, n_workers=1, threads_per_worker=8)
     bp = [datetime.datetime(1991, 1, 1), datetime.datetime(2000, 12, 31)]
     tr = [datetime.datetime(1991, 1, 1), datetime.datetime(2025, 12, 31)]
 
@@ -38,18 +38,20 @@ def run():
     # calc_indice_r = robjects.globalenv['calc_indice']
 
     filtered = eca_indices.Indice
-    # filtered = [x for x in filtered if x.indice_name == "id"]
+    # filtered = [x for x in filtered if x.indice_name == "tg10p"]
     for ind in filtered:
         print(ind.indice_name)
         out_f = f"netcdf_files/output/icclim_{ind.indice_name}_1991_2100.nc"
         try:
             climpact_files = glob.glob(
-                f"/Users/aoun/workspace/climpact-master/www/output/gridded/{ind.indice_name}_MON*.nc")
+                f"/Users/aoun/workspace/climpact-master/www/output/gridded/{ind.indice_name}_MON*.nc"
+            )
             if len(climpact_files) > 1:
                 print(
-                    f"Climpact has multiple files for {ind.indice_name}. Taking the first one")
+                    f"Climpact has multiple files for {ind.indice_name}. Taking the first one"
+                )
             climpact_result = xr.open_dataset(climpact_files[0])
-        except Exception as e:
+        except Exception:
             print(ind.indice_name + " ignored")
             # ignore this indice
             continue
@@ -59,17 +61,15 @@ def run():
         icclim_result = icclim.indice(
             indice_name=ind.indice_name,
             in_files=files,
-            var_name=ind.variables,
             slice_mode="MS",
             base_period_time_range=bp,
             time_range=tr,
             out_file=out_f,
             transfer_limit_Mbytes=200,
-            out_unit=out_unit
+            out_unit=out_unit,
         )
         icc_ind = icclim_result[ind.indice_name]
-        climp_ind = convert_unit(climpact_result[ind.indice_name],
-                                 ind.indice_name)
+        climp_ind = convert_unit(climpact_result[ind.indice_name], ind.indice_name)
 
         icc = icc_ind.mean(dim="lat").mean(dim="lon")
         climp = climp_ind.mean(dim="lat").mean(dim="lon")
@@ -77,9 +77,9 @@ def run():
         fig, ax = plt.subplots(1, 1, figsize=(20, 10))
         ax.plot(climp.time, climp, linewidth=2, label="climpact")
         ax.plot(icc.time, icc, linewidth=2, label="icclim")
-        plt.legend(prop={'size': 10})
+        plt.legend(prop={"size": 10})
         plt.title(ind.indice_name)
-        plt.savefig(f'comparison/{ind.indice_name}.png')
+        plt.savefig(f"comparison/{ind.indice_name}.png")
 
     time_elapsed = time.perf_counter() - time_start
     print(time_elapsed, " secs")
