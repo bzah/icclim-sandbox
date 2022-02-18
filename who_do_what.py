461632d4bde8a07424c657a418744e6234146b1d
abel <aoun@cerfacs.fr>
2022-02-18 12:33:46 +0100

    FIX: string casting

--- who_do_what.py
+++ who_do_what.py
@@ -1,6 +1,9 @@
 """
 Script to compare indices provided by xclim vs the ones of clix-meta.
 The output is printed in stdout.
+Needs icclim and xclim to be installed.
+
+Run it with `python -m who_do_what`
 """
 import xclim.indicators.seaIce
 import xclim.indicators.atmos
@@ -33,25 +36,26 @@ def main():
     for xc in xclim_indices:
         if xc[1] != "no_standard_name":
             if xc[1].upper() not in list(
-                    map(lambda clix: clix[1].upper(), clix_indices)):
+                    map(lambda clix: str(clix[1]).upper(), clix_indices)):
                 in_xclim_not_in_clix.append(xc)
             else:
                 in_both.append((xc, "by standard_name"))
         else:
             if xc[0].upper() not in list(
-                    map(lambda clix: clix[0].upper(), clix_indices)):
+                    map(lambda clix: str(clix[0]).upper(), clix_indices)):
                 in_xclim_not_in_clix.append(xc)
             else:
                 in_both.append((xc, "by short name"))
     for clix in clix_indices:
         if clix[1] != "no_standard_name":
-            if clix[1].upper() not in list(
-                    map(lambda ind: ind[1].upper(), xclim_indices)):
+            if str(clix[1]).upper() not in list(
+                    map(lambda ind: str(ind[1]).upper(), xclim_indices)):
                 in_clix_not_in_xclim.append(clix)
         else:
-            if clix[0].upper() not in list(
-                    map(lambda ind: ind[0].upper(), xclim_indices)):
+            if str(clix[0]).upper() not in list(
+                    map(lambda ind: str(ind[0]).upper(), xclim_indices)):
                 in_clix_not_in_xclim.append(clix)
+    # TODO: Improve output to create a csv or something similar
     print(f"in_xclim_not_in_clix indices: {in_xclim_not_in_clix}")
     print(f"in_clix_not_in_xclim indices: {in_clix_not_in_xclim}")
     print(f"in_both indices: {in_both}")
